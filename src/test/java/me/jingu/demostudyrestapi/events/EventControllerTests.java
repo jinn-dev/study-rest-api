2915c33f97ba7143f659b87dc48bd049d718bd9b
jinn-dev <gsbyyy@naver.com>
2020-05-21 00:45:58 +0900

    polishing

--- src/test/java/me/jingu/demostudyrestapi/events/EventControllerTests.java
+++ src/test/java/me/jingu/demostudyrestapi/events/EventControllerTests.java
@@ -1,7 +1,9 @@
 package me.jingu.demostudyrestapi.events;
 
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -14,11 +16,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest //Web관련 bean만 등록하고 Repository는 등록 안될 수 있음 -> @MockBean으로 따로 선언
@@ -30,6 +34,8 @@ class EventControllerTests {
 	@Autowired
 	ObjectMapper objectMapper;
 	
+	//Mockito를 사용해서 mock 객체를 만들고 빈으로 등록해준다.
+	//(주의)기존빈을 해당 테스트빈이 대체한다.
 	@MockBean
 	EventRepository eventRepository;
 	
@@ -61,7 +67,10 @@ class EventControllerTests {
 				.content(objectMapper.writeValueAsString(event)))
 		.andDo(print())	//어떤 요청과 어떤 응답 받았는지 확인 가능
 		.andExpect(status().isCreated())
-		.andExpect(jsonPath("id").exists()); //TODO id는 DB에 들어갈 때 자동 생성된 값으로 나오는지 확인
+		.andExpect(jsonPath("id").exists()) //TODO id는 DB에 들어갈 때 자동 생성된 값으로 나오는지 확인
+		.andExpect(header().exists(HttpHeaders.LOCATION))
+		.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+		;
 	}
 
 }
